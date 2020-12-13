1465907a2e8cfc837ea3379f7680d50cacef9dfb
Ruslan <ruslan.ogarkov88@gmail.com>
2020-12-14 01:22:59 +0200

    added config settings

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,14 +7,6 @@ import { async } from 'regenerator-runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -32,7 +24,7 @@ const controlRecipes = async function () {
     // 3) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.error(err);
+    console.log(err);
   }
 };
 
